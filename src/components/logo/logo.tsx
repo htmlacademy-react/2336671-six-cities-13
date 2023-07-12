8dd64eda9875bee52ca1f6af985cd434950197bd
yesiammark <yesiammark@gmail.com>
2023-07-12 16:28:29 +0300

    Добавляет Link в лого в хедере и футере

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
+
 function Logo(): JSX.Element {
   return (
-    <a className="header__logo-link header__logo-link--active">
+    <Link className="header__logo-link header__logo-link--active" to="/">
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-    </a>
+    </Link>
   );
 }
 
