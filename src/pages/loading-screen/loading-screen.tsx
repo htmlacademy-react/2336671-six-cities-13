285d12ab24237a7cc2ddf5f89aab8a141c32f34b
yesiammark <yesiammark@gmail.com>
2023-08-08 16:42:56 +0300

    Добавляет спиннер

--- src/pages/loading-screen/loading-screen.tsx
+++ src/pages/loading-screen/loading-screen.tsx
@@ -1,6 +1,10 @@
+import styles from './loading-screen.module.css';
+
 function LoadingScreen():JSX.Element {
   return (
-    <p>Loading...</p>
+    <div className={styles['pos-center']}>
+      <div className={styles['loader']}></div>
+    </div>
   );
 }
 
