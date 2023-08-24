c470414684b99de885ed54096d4dcec591e2315a
yesiammark <yesiammark@gmail.com>
2023-08-24 11:19:21 +0300

    Добавляет тесты

---
+++ src/components/favorites-list/favorites-list.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import FavoritesList from './favorites-list';
+import { getFakeFavorite } from '../../utils/mocks';
+import { withHistory, withStore } from '../../utils/mock-component';
+
+describe('Component: favorites-list', () => {
+  it('Should render correct', () => {
+
+    const fakeFavoriteOffer = getFakeFavorite();
+    const city = fakeFavoriteOffer.city;
+    const favorites = [fakeFavoriteOffer];
+    const favoritesPlacesContainerTestId = 'favorites-places-container';
+    const { withStoreComponent } = withStore(<FavoritesList city={city.name} favoriteOffers={favorites}/>);
+    const preparedComponent = withHistory(withStoreComponent);
+
+    render(preparedComponent);
+    const favoritesPlacesContainer = screen.getByTestId(favoritesPlacesContainerTestId);
+
+    expect(favoritesPlacesContainer).toBeInTheDocument();
+    expect(favoritesPlacesContainer.childElementCount).toBe(favorites.length);
+  });
+});
